a05d62cd4d0de65b5bb6df762c6776e133793f22
pepper-0 <143720331+pepper-0@users.noreply.github.com>
2025-10-06 02:59:36 -0400

    added sidebar nav for information access + started playing around w/ frontend appearance

--- public/script.js
+++ public/script.js
@@ -2,25 +2,52 @@
 - includes lots of notes for learning w/ json and fetch api
 */
 document.getElementById("plant-form").addEventListener("submit", plantSubmit); // to submit your plant diary entry
+document.getElementsByClassName("sidebar-button");
+for (let btn of document.getElementsByClassName("sidebar-button")) {
+    btn.addEventListener("click", toggleSidebar);
+}
 var entries = document.getElementById("entries"); // to update the entries shown in the page
-
-const tiles = document.getElementsByClassName("tile").addEventListener("click", openPlant);
-const allPlants = []; // will hold all plant entries
-// TODO: these are the tiles that also hold the plants; we need to link them together.
-    // do so with loop:
-// for (let tile of tiles) {
-//     const tileID = tile.id;
-//     // Find the plant(s) for this tile
-//     const plantsForTile = allPlants.filter(plant => plant.tileID === tileID);
-//     // Display plant info in the tile
-//     plantsForTile.forEach(plant => {
-//         tile.innerHTML += `<h3>${plant.title}</h3><p>${plant.entry}</p><small>${plant.date}</small>`;
-//     });
-// }
-
-
+var allPlants = []; // global array to hold all plant entries
+var displayedPlants = [16]; // global array to hold currently displayed plant entries (for filtering, searching, etc)
 
 updateEntriesInPage(); // call function to load existing entries when page loads
+
+const tiles = document.getElementsByClassName("tile"); // static frontend array containing all elements that are tiles (16)
+
+function toggleSidebar() {
+    var sidebar = document.getElementById("sidebar");
+    var main = document.getElementById("main");
+    if (sidebar.style.width === "50%") {
+        sidebar.style.width = "0";
+        main.style.marginLeft = "0";
+    } else {
+        sidebar.style.width = "50%";
+        main.style.marginLeft = "50%";
+    }
+}
+
+// TODO: these are the tiles that also hold the plants; we need to link them together.
+for (let i = 0; i < tiles.length; i++) { // go through 16 tiles
+    const tileID = tiles[i].id; // get tile id
+    const correspondingPlant = allPlants.filter(plant => plant.tileID === tileID); // get plant corresponding to this tile; therefore you can link it to the frontend
+    if (correspondingPlant.length > 0) { // if there is a plant for this tile
+        // set up the tile with the plant info
+        displayedPlants[i] = correspondingPlant[0]; // add to displayedPlants array
+    }
+    tiles[i].addEventListener("click", () => {
+        openPlant(tileID);
+    });
+}
+
+function openPlant(tileID) {
+    const plant = displayedPlants[tileID]; // get plant corresponding to this tile
+    if (!plant) {
+        console.log("no plant for this tile");
+        return;
+    } else {
+        console.log(`Title: ${plant.title}\nEntry: ${plant.entry}\nDate: ${plant.date}`);
+    }
+}
 
 async function plantSubmit(e) {
     e.preventDefault(); // revent page from reloading
@@ -50,7 +77,7 @@ async function plantSubmit(e) {
 
 async function updateEntriesInPage() {
     const response = await fetch("/plants"); // send request to /plant endpoint in server.js (express server)
-    const allPlants = await response.json(); // get json data from response
+    allPlants = await response.json(); // get json data from response
 
     entries.innerHTML = ""; // reset 
 
